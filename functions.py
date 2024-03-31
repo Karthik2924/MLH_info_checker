9292052b7f714a84ee569ae1233ad7acdf4ff382
Karthik2924 <54316824+Karthik2924@users.noreply.github.com>
2024-03-31 12:03:52 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -18,6 +18,26 @@ GOOGLE_API_KEY = 'AIzaSyBYv4rXJa5ft99khetg4PNEnYGE2qOd3pg'
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+# from IPython.display import display
+# from IPython.display import Markdown
+
+#pip install google-search python
+from googlesearch import search
+
+import bs4 as bs
+import urllib.request
+
+
+## Import the key safely
+GOOGLE_API_KEY = 'AIzaSyBYv4rXJa5ft99khetg4PNEnYGE2qOd3pg'
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+
 
 # def to_markdown(text):
 #   text = text.replace('•', '  *')
@@ -33,7 +53,17 @@ def process_url(url):
     return txt
 
 
-def llm_output(txt,type):
-    prompt1 = "Give a one line explanation of the main idea of the following content"
-    prompt2 = "List the claims "
-    response = model.generate_content()
+def llm_define_input(txt):
+    prompt1 = "Give a one line explanation of the main idea of the following content "
+    # prompt2 = "List the claims in the following text "
+    response = model.generate_content(prompt1 + txt)
+    return response.candidates[0].content.parts[0].text
+    
+def llm_output(txt,query):
+    prompt= " The above is an information source, does the main idea of the source verify the following query "
+    # prompt2 = "List the claims in the following text "
+    response = model.generate_content(txt + prompt + query)
+    return response.candidates[0].content.parts[0].text
+
+# def llm_aggregate(l):
+    
